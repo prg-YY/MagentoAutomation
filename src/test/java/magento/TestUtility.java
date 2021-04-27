f8b5870628cb8db5cfedff3b589e0f906091b76e
abdusamad mejid <abdusamad.mejid@gmail.com>
2021-04-27 20:08:35 +0400

    Test Utility3

---
+++ src/test/java/magento/TestUtility.java
@@ -0,0 +1,50 @@
+package magento;
+
+import configutiity.ApplicationConfig;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+
+public class TestUtility {
+    private int timeout = Integer.parseInt(ApplicationConfig
+            .readConfigProperties("config.properties", "timeout"));
+    private WebDriver driver;
+
+    public TestUtility(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitForElementPresent(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, timeout);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void sleep(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void takeScreenShot(String fileName) {
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File screenShotFile = screenshot.getScreenshotAs(OutputType.FILE);
+        String folder = ApplicationConfig.readConfigProperties("config.properties",
+                "imagefolder");
+        File finalFile = new File(folder + File.separator + fileName);
+        try {
+            FileUtils.copyFile(screenShotFile, finalFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void waitForAlertPresent(){
+        WebDriverWait wait=new WebDriverWait(driver,timeout);
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+}
