98d10583de5fae3c25b264cd5e600dc269a3d1ac
abdusamad mejid <abdusamad.mejid@gmail.com>
2021-05-05 02:53:20 +0400

    CreateAccountPage update4

--- src/main/java/com/seleniummaster/magento/backendpages/customerpages/UpdateCustomerGroupPage.java
+++ src/main/java/com/seleniummaster/magento/backendpages/customerpages/UpdateCustomerGroupPage.java
@@ -11,18 +11,29 @@ import org.openqa.selenium.support.ui.Select;
 public class UpdateCustomerGroupPage extends TestBasePage {
     WebDriver driver;
     TestUtility utility;
+    @FindBy(xpath = "//*[@id=\"nav\"]/li/a/span")
+    WebElement customerLink;
     @FindBy(xpath = "//*[@id=\"nav\"]/li/ul/li[2]/a/span")
     WebElement customerGroupLink;
-    @FindBy(xpath = "//button[@id=\"id_b9ba7e3d7b5f91d9347642f07c7a0de4\"]/span")
+    @FindBy(xpath = "//span[text()='Add New Customer Group']")
     WebElement addCustomerGroupLink;
     @FindBy(id = "customer_group_code")
     WebElement cus_GroupNameField;
     @FindBy(id = "tax_class_id")
     WebElement taxClassSelection;
-    @FindBy(xpath = "//*[@id=\"id_bdbbe1648a0ed3b91db8acc3ee91fe3f\"]/span/span/span")
+    @FindBy(xpath = "//span[text()='Save Customer Group']")
     WebElement saveCustomerGroup;
     @FindBy(xpath = "//*[@id=\"messages\"]/ul/li")
     WebElement successMessage;
+    // for Update Customer
+
+    @FindBy( xpath= "//*[@class=\"field-100\"]/input")
+    WebElement cus_SearchBox;
+    @FindBy( xpath= "//span[text()='Search']")
+    WebElement searchButton;
+    @FindBy(xpath = "//*[@id=\"customerGroupGrid_table\"]/tbody/tr")
+    WebElement customerGroupRow;
+    ////div[@class="hor-scroll"]/table/tbody/tr
 
 
     //combine driver
@@ -35,6 +46,10 @@ public class UpdateCustomerGroupPage extends TestBasePage {
     }
 
     //method for each action
+    public void clickOnCustomersLink(){
+        utility.waitForElementPresent(customerLink);
+        customerLink.click();
+    }
     public void clickOnCustomerGroupLink(){
         utility.waitForElementPresent(customerGroupLink);
         customerGroupLink.click();
@@ -43,7 +58,7 @@ public class UpdateCustomerGroupPage extends TestBasePage {
         utility.waitForElementPresent(addCustomerGroupLink);
         addCustomerGroupLink.click();
     }
-    public void enterCustomerName(String groupName){
+    public void enterCustomerGroupName(String groupName){
         utility.waitForElementPresent(cus_GroupNameField);
         cus_GroupNameField.sendKeys(groupName);
     }
@@ -60,12 +75,47 @@ public class UpdateCustomerGroupPage extends TestBasePage {
         utility.waitForElementPresent(successMessage);
         return successMessage.isDisplayed();
     }
+    //method for update customer actions     ************
+    public void enterGroupNameToSearchBox(String groupName){
+        utility.waitForElementPresent(cus_SearchBox);
+        cus_SearchBox.sendKeys(groupName);
+    }
+    public void clickOnSearchButton(){
+        utility.waitForElementPresent(searchButton);
+        searchButton.click();
+    }
+    public void clickOnCustomerGroupRow(){
+        utility.waitForElementPresent(customerGroupRow);
+        customerGroupRow.click();
+    }
     // method for Adding Customer Group
     public void addCustomerGroup(String groupName){
+        clickOnCustomersLink();
         clickOnCustomerGroupLink();
         clickOnAddCustomerGroup();
-        enterCustomerName(groupName);
+        enterCustomerGroupName(groupName);
         taxClassSelection();
+        clickOnSaveButton();
+        successMessageDisplayed();
+    }
+// update customer method
+    public void searchCustomerForUpdate(String cus_GroupName){
+        clickOnCustomersLink();
+        utility.sleep(5);
+        clickOnCustomerGroupLink();
+        utility.sleep(5);
+        cus_SearchBox.click();
+        utility.sleep(5);
+        enterGroupNameToSearchBox(cus_GroupName);
+        utility.sleep(5);
+        clickOnSearchButton();
+        utility.sleep(5);
+        clickOnCustomerGroupRow();
+        utility.sleep(5);
+    }
+    public void updateCustomerGroup(String customerGroupName){
+        cus_GroupNameField.clear();
+        enterCustomerGroupName(customerGroupName);
         clickOnSaveButton();
         successMessageDisplayed();
     }
--- src/test/java/com/unitedcoder/regressiontest/customermoduletestrunner/CustomerModuleTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/customermoduletestrunner/CustomerModuleTestRunner.java
@@ -3,6 +3,7 @@ package com.unitedcoder.regressiontest.customermoduletestrunner;
 import com.seleniummaster.magento.backendpages.BackEndLogin;
 import com.seleniummaster.magento.backendpages.customerpages.CustomerPage;
 import com.seleniummaster.magento.backendpages.customerpages.UpdateCustomerGroupPage;
+import com.seleniummaster.magento.testdata.TestDataHolder;
 import com.seleniummaster.magento.utility.Log;
 import com.seleniummaster.magento.utility.TestBasePage;
 
@@ -10,6 +11,10 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 public class CustomerModuleTestRunner extends TestBasePage {
     static UpdateCustomerGroupPage customerGroupPage;
@@ -37,14 +42,34 @@ public class CustomerModuleTestRunner extends TestBasePage {
         Assert.assertTrue(customerPage.verifySuccessMessage());
 
     }
+
+    TestDataHolder holder;
+    Random ran=new Random();
     @Test(description = "User Should be able to add new customer Group")
-    public void addCustomerGroup(String groupName){
+    public void addCustomerGroup(){
+        int r=ran.nextInt(50);
         customerGroupPage=new UpdateCustomerGroupPage(driver);
-        customerGroupPage.addCustomerGroup(groupName);
+        holder=new TestDataHolder();
+        customerGroupPage.addCustomerGroup(groupName+r);
+        holder.setCustomerGroupName(groupName+r);
+        prop.setProperty("cus_GroupName_Update",holder.getCustomerGroupName());
+        try {
+            prop.store(new FileOutputStream("C:\\Users\\Admin\\ameap_team1\\config.properties"),
+                    "holdingGroupData");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Assert.assertTrue(customerGroupPage.successMessageDisplayed());
-
     }
-
+    @Test(description = "User should be able to Update Customer Group")
+    public void updateCustomerGroup(){
+        String newGroupName= prop.getProperty("cus_GroupName_Update");
+        customerGroupPage.searchCustomerForUpdate(newGroupName);
+        int r= ran.nextInt(300);
+        customerGroupPage.updateCustomerGroup(groupName+r);
+        Assert.assertTrue(customerGroupPage.successMessageDisplayed());
+    }
 
     @AfterClass
     public static void close(){
