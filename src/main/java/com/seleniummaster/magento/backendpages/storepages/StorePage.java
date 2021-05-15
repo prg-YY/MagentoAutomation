67b16a746bd2dce09562db2dc71b5fee458bb968
14437 <abkeram5@gmail.com>
2021-05-15 13:02:37 -0400

    26

--- src/main/java/com/seleniummaster/magento/backendpages/storepages/StorePage.java
+++ src/main/java/com/seleniummaster/magento/backendpages/storepages/StorePage.java
@@ -1,10 +1,15 @@
 package com.seleniummaster.magento.backendpages.storepages;
 
 import com.seleniummaster.magento.utility.TestBasePage;
+import com.seleniummaster.magento.utility.TestUtility;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class StorePage extends TestBasePage {
+    WebDriver driver;
+    TestUtility utility;
+
     @FindBy(xpath = "(//span[text()='Create Store'])[1]")
     WebElement CreateStoreButton;
 
@@ -14,8 +19,16 @@ public class StorePage extends TestBasePage {
     @FindBy(xpath = "(//span[text()='Create Store View'])[1]")
     WebElement CreateStoreviewButton;
 
+    @FindBy(css = "#website_name")
+    WebElement NameTextBox;
 
+    @FindBy(css = "#website_code")
+    WebElement CodeTextBox;
 
+    @FindBy(css = "#website_sort_order")
+    WebElement SortOrderTextBox;
 
+    @FindBy(css = "(//span[text()='Save Website'])[1]")
+    WebElement SaveButton;
 
 }
