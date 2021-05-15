ff081f88be10b02c846c0b8145d2f7dd9639a935
14437 <abkeram5@gmail.com>
2021-05-15 14:22:48 -0400

    27

--- src/main/java/com/seleniummaster/magento/backendpages/storepages/StorePage.java
+++ src/main/java/com/seleniummaster/magento/backendpages/storepages/StorePage.java
@@ -10,11 +10,11 @@ public class StorePage extends TestBasePage {
     WebDriver driver;
     TestUtility utility;
 
+    @FindBy(xpath = "(//span[text()='System'])[1]")
+    WebElement SystemButton;
+
     @FindBy(xpath = "(//span[text()='Create Store'])[1]")
     WebElement CreateStoreButton;
-
-    @FindBy(xpath = "(//span[text()='Create Website'])[1]")
-    WebElement CreateWebsiteButton;
 
     @FindBy(xpath = "(//span[text()='Create Store View'])[1]")
     WebElement CreateStoreviewButton;
--- src/main/java/com/seleniummaster/magento/backendpages/storepages/WebsitePage.java
+++ src/main/java/com/seleniummaster/magento/backendpages/storepages/WebsitePage.java
@@ -1,4 +1,19 @@
 package com.seleniummaster.magento.backendpages.storepages;
 
+import com.seleniummaster.magento.utility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class WebsitePage {
+    WebDriver driver;
+    TestUtility utility;
+
+    @FindBy(xpath = "(//span[text()='System'])[1]")
+    WebElement SystemButton;
+
+    @FindBy(xpath = "(//span[text()='Create Website'])[1]")
+    WebElement CreateWebsiteButton;
+
+
 }
