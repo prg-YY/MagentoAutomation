372ea392d22b089b07cb8da71883d9eabde48afb
abdusamad mejid <abdusamad.mejid@gmail.com>
2021-05-19 09:40:38 +0400

    sales order page step definition add method to Test base

---
+++ src/test/java/com/unitedcoder/regressiontest/cucumber/sales/CreateOrder.java
@@ -0,0 +1,19 @@
+package com.unitedcoder.regressiontest.cucumber.sales;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreateOrder {
+    @When("click on create order")
+    public void clickOnCreateOrder() {
+    }
+
+    @And("Add products to order and choose payment method and shipment and click submit order button")
+    public void addProductsToOrderAndChoosePaymentMethodAndShipmentAndClickSubmitOrderButton() {
+    }
+
+    @Then("verify the order successfully created")
+    public void verifyTheOrderSuccessfullyCreated() {
+    }
+}
