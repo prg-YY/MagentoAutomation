59a7365e49ded095fcd5764c7caacb430a2b0c81
abdusamad mejid <abdusamad.mejid@gmail.com>
2021-05-30 23:14:32 +0400

    project ni retlash

---
+++ src/main/java/com/seleniummaster/magento/database/ConnectionManager.java
@@ -0,0 +1,74 @@
+package com.seleniummaster.magento.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+    //create a method to connect database
+    public static Connection connectToDataBaseServer(String dburl,String dbPort,
+                                                     String dbUserName,String dbPassword,
+                                                     String defaultDatabase,ConnectionType connectionType) {
+        Connection connection=null;
+        String JTDS_Driver="net.sourceforge.jtds.jdbc.Driver";//sql
+        String MYSQL_Driver="com.mysql.cj.jdbc.Driver";//my sql
+        switch (connectionType){
+            case MSSQLSERVER:
+                try {
+                    Class.forName(JTDS_Driver);//load the drive in to memory
+                } catch (ClassNotFoundException e) {
+                    new RuntimeException("Please check driver information");
+                }
+                String connectionUrl="jdbc:jtds:sqlserver://" + dburl + ":"
+                        + ";databaseName=" + defaultDatabase;
+                try {
+                    connection= DriverManager.getConnection(connectionUrl,dbUserName,dbPassword);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case MYSQLServer:
+                // Class.forName(MYSQL_Driver).newInstance();
+                try {
+                    Class.forName(MYSQL_Driver).newInstance();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                String mySqlConnection="jdbc:mysql://"+dburl+":"+dbPort+"/"+defaultDatabase;
+                try {
+                    connection=DriverManager.getConnection(mySqlConnection,dbUserName,dbPassword);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(mySqlConnection);
+                break;
+            default:
+                System.out.println("You need to specify data base connection type(MSSQL or MySQL)");
+        }
+        //verify if the connection is established or not
+        try {
+            if(!connection.isClosed()){
+                System.out.println("Data base connection is established");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public static void closeDataBaseConnection(Connection connection){
+        try {
+            if(connection.isClosed()){
+                System.out.println("Data base connection has been closed");
+            }else{
+                connection.close();
+                System.out.println("connection is closed!");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/seleniummaster/magento/testdata/test.java
+++
@@ -1,4 +0,0 @@
-package com.seleniummaster.magento.testdata;
-
-public class test {
-}
