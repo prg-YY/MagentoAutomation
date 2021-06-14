a2cda520fcb8a7ad67f8263c31cd7d6d83011bc6
prg0615 <parguzatt@gmail.com>
2021-06-14 21:12:10 +0200

    data base test  cucumber8

--- src/test/java/com/unitedcoder/regressiontest/cucumber/CucumberTestRunner.java
+++ src/test/java/com/unitedcoder/regressiontest/cucumber/CucumberTestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"json:target/CucumberReportingModule-Sales/salesModule.json"},
         features = {"src/test/resources"},
-        tags = "@ReportingModuleShoppingCartProducts,@ReportingModule,@ReportingModuleSales",
+        tags = "@MarketingModule",
         monochrome = true
 )
 public class CucumberTestRunner extends TestBasePage {
