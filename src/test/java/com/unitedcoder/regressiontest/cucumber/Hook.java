90ebbe97c47e5c139b9b53af5ab664976f5ac036
prg0615 <parguzatt@gmail.com>
2021-06-14 20:30:16 +0200

    data base test  cucumber6

--- src/test/java/com/unitedcoder/regressiontest/cucumber/Hook.java
+++ src/test/java/com/unitedcoder/regressiontest/cucumber/Hook.java
@@ -27,7 +27,7 @@ public class Hook extends TestBasePage {
       storeDashboardPage.clickLogOutLink();
     }
 
-   @After (order = 0,value = "@SeeProductReviewsReport"+"@CouponsUsageReport" + "@SeeProductsDownloadsReport")
+   @After (order = 0,value = "@SeeProductReviewsReport or "+"@CouponsUsageReport or " + "@SeeProductsDownloadsReport")
     public void tearDown() {
         driver.close();
         driver = null;
